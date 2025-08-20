ecf544a165fc0a607554443e6a94ca34ed5051ac
amrita-srivastava10 <amrita.sri@auctech.in>
2025-08-20 19:12:07 +0530

    condition  button completed

--- src/components/NotificationBell.jsx
+++ src/components/NotificationBell.jsx
@@ -119,7 +119,7 @@ const NotificationBell = () => {
   return (
     <div className="relative hidden md:block" ref={bellRef}>
       {/* Notification Bell Button */}
-      <div className="relative">
+      <div className=" relative">
         <button
           className="relative bg-[#3c83f8] hover:from-indigo-600 hover:via-purple-600 hover:to-pink-600 text-white p-3 rounded-full shadow-lg hover:shadow-xl transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
