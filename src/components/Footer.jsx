4b5d3c50299bebf73ac53f584906763affa494e6
Abhishekyadavauctech <abhishekauctech@gmail.com>
2025-09-11 18:13:46 +0530

    footer-correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
   return (
     <>
       {/* Footer */}
-      <footer id="contact" className="bg-gray-900 text-white py-16">
+      <footer id="contact" className="bg-gray-900 text-white md:py-16 py-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 md:max-w-[85%] ">
           <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div>
