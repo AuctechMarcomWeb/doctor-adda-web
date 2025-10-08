f2e76129717633556544ccf8de4015fa899fdf68
unknown <rahulgautam8933@gmail.com>
2025-10-08 12:58:07 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
     try {
       // Send order data to backend
       const res = await axios.post(
-        "https://doctors-adda-back.onrender.com/api/appointment/ccvanuePayment", 
+        "http://localhost:5000/api/appointment/ccvanuePayment", 
         orderData,
         { headers: { "Content-Type": "application/json" } }
       );
